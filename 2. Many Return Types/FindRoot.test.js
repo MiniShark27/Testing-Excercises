cecb2b3d15008c3241c5daf6dae527dbe3fd94ad
MiniShark27 <kobifalus@gmail.com>
2022-03-30 11:21:38 -0400

    Rename Files

--- 2. Many Return Types/FindRoot.test.js
+++ 2. Many Return Types/FindRoot.test.js
@@ -1,6 +1,4 @@
-const { test, expect } = require("@jest/globals");
-
-const findRoot = require("./Complex").findRoot;
+const findRoot = require("./FindRoot").findRoot;
 
 /*
   This part of the exercise is to create tests using different Jest Matchers
@@ -23,7 +21,7 @@ describe("findRoot Testing", () => {
 /*
   Exercise 2 (Do Below):
     Brainstorm the different cases that could be tested (should be at least 3)
-      Try to use a different function than the one in Complex.js
+      Try to use a different function than the one in FindRoot.js
     Write a test for each case in the above describe block
     Run `npm test` to run the tests and verify they pass
 */
