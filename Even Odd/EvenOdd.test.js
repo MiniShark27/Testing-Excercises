8c85d39d0f78f92a7a9db84c59430c1bfb24880a
MiniShark27 <kobifalus@gmail.com>
2022-03-29 23:19:36 -0400

    use expect instead of assert.equal

--- Even Odd/EvenOdd.test.js
+++ Even Odd/EvenOdd.test.js
@@ -4,8 +4,6 @@
   After that is the first excercist (making a test suite for the isOdd function)
 */
 
-// Need to import/require assert to use it (not imported by default)
-let assert = require("assert");
 // Remember to import the function you are testing
 let isEven = require("./EvenOdd.js").isEven;
 
@@ -14,12 +12,12 @@ describe("isEven", function () {
   // Use the test() function to define a test
   test("should return true if an even number is passed", function () {
     // To verify that the function returns true, we use assert.equal()
-    assert.equal(isEven(2), true);
+    expect(isEven(2)).toBe(true);
   });
 
   // Note: it() is an alias for test()
   it("should return false if an odd number is passed", function () {
-    assert.equal(isEven(1), false);
+    expect(isEven(1)).toBe(false);
   });
 });
 
@@ -27,7 +25,6 @@ describe("isEven", function () {
   Excercise 1: 
     Write 2 tests for isOdd()
     Put those tests in a describe block
-    Note: assert is already imported, but isOdd() is not
 */
 
 //CODE START
